2566be5fbbc5ac5a5c5442dc753eb28dbf606785
fagnyeqi <103356538+Yeqi-Fang@users.noreply.github.com>
2023-04-22 19:06:26 +0800

    mhgfds

--- confusion_matrix.py
+++ confusion_matrix.py
@@ -2,7 +2,10 @@ import numpy as np
 from sklearn.metrics import confusion_matrix
 import seaborn as sns
 import matplotlib.pyplot as plt
-a = np.array([[54, 1], [5, 43]])
+a = np.array([
+    [54, 1],
+    [5, 43]
+])
 
 # plot confusion matrix
 sns.heatmap(a, annot=True)
