42d7b221934614d2874a6847c4ccbc80514117d2
fagnyeqi <103356538+Yeqi-Fang@users.noreply.github.com>
2023-05-19 08:28:59 +0800

    t43

--- confusion_matrix.py
+++ confusion_matrix.py
@@ -3,10 +3,13 @@ from sklearn.metrics import confusion_matrix
 import seaborn as sns
 import matplotlib.pyplot as plt
 a = np.array([
-    [54, 1],
-    [5, 43]
+    [37, 10, 0, 0],
+    [9, 20, 2, 0],
+    [1, 3, 9, 1],
+    [0, 0, 2, 6]
 ])
 
 # plot confusion matrix
 sns.heatmap(a, annot=True)
-plt.show()
+# plt.show()
+plt.savefig('confusion_matrix.png', dpi=600)
